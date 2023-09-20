ca2e832cf2f30db8e259351633ce92982b9203ff
khaoshimself <bryan.torres9023@gmail.com>
2023-09-20 12:19:37 -0500

    HFHFFHFHFHFHFHF

--- webScraper.py
+++ webScraper.py
@@ -1,1 +1,12 @@
-print("hello world howdy can you work please testingngngngngngngnng")
+from bs4 import BeautifulSoup
+import requests
+
+try:
+    source = requests.get('https://realpython.github.io/fake-jobs/')
+    source.raise_for_status() #used to raise an error if url is not found
+
+    soup = BeautifulSoup(source.text, 'html.parser') 
+    print(soup)
+
+except Exception as e:
+    print(e)
